31fda141cacc5bc57ffd2f4759edf3c85a009136
hyeonwookim <wooin.dev@gmail.com>
2023-09-14 09:36:21 +0900

    q15649 순열 풀이중

--- src/baekjoon/q15649/Main.java
+++ src/baekjoon/q15649/Main.java
@@ -18,8 +18,8 @@ class Solution {
     int maxNum;
     int maxCnt;
 
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     void solution() throws IOException {
 
@@ -28,40 +28,36 @@ class Solution {
         maxNum = Integer.parseInt(nums[0]);
         maxCnt = Integer.parseInt(nums[1]);
 
-        for (int i = 1; i <= maxNum; i++) {
-            int[] used = new int[maxNum + 1]; //숫자가 인덱스랑 같게 +1 해주기
-            used[i]=1;
-            printNum(i, 1, used);
-            bw.newLine();
-        }
-
+        int[] used = new int[maxNum + 1]; //숫자가 인덱스랑 같게 +1 해주기
+        permutation(0, used);
 
         bw.close();
         br.close();
     }
 
-    private void printNum(int i, int cnt, int[] used) throws IOException {
+    private void permutation(int cnt, int[] used) throws IOException {
+        // 카운트가 맥스 넘어가면 끝. 처음엔 0. 출력된 숫자가 곧 cnt.
+        if (cnt >= maxCnt) {
+            bw.newLine();
+            return;
+        } else {
 
-        bwWriteIntToStr(i);
-
-        bw.write(" ");
-
-        for (int j = 0; j < maxCnt; j++) {
-            for (int k = i + 1; k <= maxNum; k++) {
-                printNum(k, cnt+1, used);
+            // 출력할 숫자가 used에 있는지 확인
+            for (int i = 1; i <= maxNum; i++) {
+                // 있으면 다음 숫자로 넘기기
+                if (used[i] == 1) continue;
+                // 없으면 출력하고 used에 표시
+                used[i] = 1;
+                bwWriteIntToStr(i);
+                bw.write(" ");
+                permutation(cnt + 1, used);
+                used[i] = 0;
             }
         }
-
-
     }
 
     void bwWriteIntToStr(int i) throws IOException {
         bw.write(String.valueOf(i));
-        bw.newLine();
+//        bw.newLine();
     }
 }
-
-
-
-
-
